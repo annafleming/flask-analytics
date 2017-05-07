42165e73c89cd8b501264ba2234b163a55152086
Anna Fleming <afleming@petsafe.net>
2017-05-07 18:31:46 -0400

    Add navbar

--- analytics/__init__.py
+++ analytics/__init__.py
@@ -10,7 +10,7 @@ toolbar = DebugToolbarExtension()
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_by_name[config_name])
-    toolbar.init_app(app)
+    # toolbar.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint, url_prefix='/')
