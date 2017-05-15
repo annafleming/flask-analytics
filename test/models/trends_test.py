08bd8af3b51ec38faa684f754135c4355907da55
Anna Fleming <afleming@petsafe.net>
2017-05-15 09:04:30 -0400

    Fix 'fill_values_if_monthly_data_is_missing' test

--- test/models/trends_test.py
+++ test/models/trends_test.py
@@ -8,15 +8,15 @@ class TrendsTest(unittest.TestCase):
 
     def test_should_fill_in_values_if_monthly_data_is_missing(self):
         ds = pd.DataFrame([["2015-07-01", 1, 1],
-                           ["2015-07-03", 0, 3]], columns=['Date', 'Finished', 'Total'])
+                           ["2015-09-01", 0, 3]], columns=['Date', 'Finished', 'Total'])
         expected_ds = pd.DataFrame([["2015-07-01", 1, 1],
-                                    ["2015-07-01", 0, 0],
-                                    ["2015-07-03", 0, 3]], columns=['Date', 'Finished', 'Total'])
+                                    ["2015-09-01", 0, 3],
+                                    ["2015-08-01", 0, 0]], columns=['Date', 'Finished', 'Total'])
         result_ds = analytics.models.trends.\
             fill_values_if_monthly_data_is_missing(ds, date_column='Date',
                                                    format="%Y-%m-%d",
                                                    values={'Finished': 0, 'Total': 0})
-        # self.assertTrue(result_ds.equals(expected_ds))
+        self.assertTrue(result_ds.equals(expected_ds))
 
     if __name__ == '__main__':
         unittest.main()
