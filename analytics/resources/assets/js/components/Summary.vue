aec9dec6699275be0299521d57a3fdba797762b4
Anna Fleming <afleming@petsafe.net>
2017-05-19 18:59:06 -0400

    Add subcomponent for Summary Vue component

--- analytics/resources/assets/js/components/Summary.vue
+++ analytics/resources/assets/js/components/Summary.vue
@@ -1,143 +1,55 @@
 <template>
-  <div>
-    <div v-for="(contents, index) in summary">
-      <div class="well">
-          <h3>Last 7 days</h3>
-          <div class="row">
-              <div class="col-lg-3 col-md-6">
-                  <div class="panel panel-primary">
-                      <div class="panel-heading">
-                          <div class="row">
-                              <div class="col-xs-3">
-                                  <i class="fa fa-comments fa-5x"></i>
-                              </div>
-                              <div class="col-xs-9 text-right">
-                                  <div class="huge">{{ contents['week']['reviews'] }}</div>
-                                  <div>New Reviews</div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <div class="col-lg-3 col-md-6">
-                  <div class="panel panel-green">
-                      <div class="panel-heading">
-                          <div class="row">
-                              <div class="col-xs-3">
-                                  <i class="fa fa-thumbs-o-up fa-5x"></i>
-                              </div>
-                              <div class="col-xs-9 text-right">
-                                  <div class="huge">{{ contents['week']['promoters'] }}</div>
-                                  <div>Promoters</div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <div class="col-lg-3 col-md-6">
-                  <div class="panel panel-yellow">
-                      <div class="panel-heading">
-                          <div class="row">
-                              <div class="col-xs-3">
-                                  <i class="fa fa-meh-o fa-5x"></i>
-                              </div>
-                              <div class="col-xs-9 text-right">
-                                  <div class="huge">{{ contents['week']['passives'] }}</div>
-                                  <div>Passives</div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <div class="col-lg-3 col-md-6">
-                  <div class="panel panel-red">
-                      <div class="panel-heading">
-                          <div class="row">
-                              <div class="col-xs-3">
-                                  <i class="fa fa-thumbs-o-down fa-5x"></i>
-                              </div>
-                              <div class="col-xs-9 text-right">
-                                  <div class="huge">{{ contents['week']['detractors'] }}</div>
-                                  <div>Detractors</div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-      <div class="well">
-          <h3>Last 30 days</h3>
-          <div class="row">
-              <div class="col-lg-3 col-md-6">
-                  <div class="panel panel-primary">
-                      <div class="panel-heading">
-                          <div class="row">
-                              <div class="col-xs-3">
-                                  <i class="fa fa-comments fa-5x"></i>
-                              </div>
-                              <div class="col-xs-9 text-right">
-                                  <div class="huge">{{ contents['month']['reviews'] }}</div>
-                                  <div>New Reviews</div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <div class="col-lg-3 col-md-6">
-                  <div class="panel panel-green">
-                      <div class="panel-heading">
-                          <div class="row">
-                              <div class="col-xs-3">
-                                  <i class="fa fa-thumbs-o-up fa-5x"></i>
-                              </div>
-                              <div class="col-xs-9 text-right">
-                                  <div class="huge">{{ contents['month']['promoters'] }}</div>
-                                  <div>Promoters</div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <div class="col-lg-3 col-md-6">
-                  <div class="panel panel-yellow">
-                      <div class="panel-heading">
-                          <div class="row">
-                              <div class="col-xs-3">
-                                  <i class="fa fa-meh-o fa-5x"></i>
-                              </div>
-                              <div class="col-xs-9 text-right">
-                                  <div class="huge">{{ contents['month']['passives'] }}</div>
-                                  <div>Passives</div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <div class="col-lg-3 col-md-6">
-                  <div class="panel panel-red">
-                      <div class="panel-heading">
-                          <div class="row">
-                              <div class="col-xs-3">
-                                  <i class="fa fa-thumbs-o-down fa-5x"></i>
-                              </div>
-                              <div class="col-xs-9 text-right">
-                                  <div class="huge">{{ contents['month']['detractors'] }}</div>
-                                  <div>Detractors</div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-    </div>
+    <div>
+    <div class="row">
+        <div class="col-lg-12">
+                <h1 class="page-header">
+                    Dashboard
+                    <small>Statistics Overview</small>
+                </h1>
+            </div>
+        </div>
 
-  </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">PetSafe</h3>
+                    </div>
+                    <div class="panel-body" v-if="summary.petsafe && summary.petsafe.week">
+                        <summary-item :summary="summary.petsafe.week" header>Last 7 days</summary-item>
+                    </div>
+
+                    <div class="panel-body" v-if="summary.petsafe && summary.petsafe.month">
+                        <summary-item :summary="summary.petsafe.month">Last 30 days</summary-item>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="panel panel-yellow">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">SportDOG</h3>
+                    </div>
+                    <div class="panel-body" v-if="summary.sportdog && summary.sportdog.week">
+                        <summary-item :summary="summary.sportdog.week" header>Last 7 days</summary-item>
+                    </div>
+
+                    <div class="panel-body" v-if="summary.sportdog && summary.sportdog.month">
+                        <summary-item :summary="summary.sportdog.month">Last 30 days</summary-item>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import SummaryItem from './SummaryItem'
+
   export default {
+    components: { SummaryItem },
     data(){
       return {
         summary: {}
@@ -146,7 +58,8 @@
 
     created(){
       axios.get('/charts/summary').then(response =>{
-        this.summary = response.data
+        this.summary = response.data;
+        console.log(this.summary);
       });
     }
 
---
+++ analytics/resources/assets/js/components/SummaryItem.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+      <div class="well">
+          <h3><slot></slot></h3>
+          <div class="row">
+              <div class="col-lg-3 col-md-6">
+                  <div class="panel panel-primary">
+                      <div class="panel-heading">
+                          <div class="row">
+                              <div class="col-xs-3">
+                                  <i class="fa fa-comments fa-5x"></i>
+                              </div>
+                              <div class="col-xs-9 text-right">
+                                  <div class="huge">{{ summary['reviews'] }}</div>
+                                  <div>New Reviews</div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-6">
+                  <div class="panel panel-green">
+                      <div class="panel-heading">
+                          <div class="row">
+                              <div class="col-xs-3">
+                                  <i class="fa fa-thumbs-o-up fa-5x"></i>
+                              </div>
+                              <div class="col-xs-9 text-right">
+                                  <div class="huge">{{ summary['promoters'] }}</div>
+                                  <div>Promoters</div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-6">
+                  <div class="panel panel-yellow">
+                      <div class="panel-heading">
+                          <div class="row">
+                              <div class="col-xs-3">
+                                  <i class="fa fa-meh-o fa-5x"></i>
+                              </div>
+                              <div class="col-xs-9 text-right">
+                                  <div class="huge">{{ summary['passives'] }}</div>
+                                  <div>Passives</div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-6">
+                  <div class="panel panel-red">
+                      <div class="panel-heading">
+                          <div class="row">
+                              <div class="col-xs-3">
+                                  <i class="fa fa-thumbs-o-down fa-5x"></i>
+                              </div>
+                              <div class="col-xs-9 text-right">
+                                  <div class="huge">{{ summary['detractors'] }}</div>
+                                  <div>Detractors</div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: [ 'summary' ]
+  }
+</script>
