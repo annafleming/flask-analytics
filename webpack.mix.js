a7c6178a7690c7168fda059bfbeb91a7ac0d2125
Anna Fleming <afleming@petsafe.net>
2017-05-19 10:11:59 -0400

    Add npm font-awesome dependency

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,6 @@ let mix = require('laravel-mix').mix;
 
 
 mix.js('analytics/resources/assets/js/app.js', 'analytics/static/js')
-.sass('analytics/resources/assets/sass/app.scss', 'analytics/static/css');
+.sass('analytics/resources/assets/sass/app.scss', 'analytics/static/css')
+.copy('./node_modules/font-awesome/fonts', 'analytics/static/vendor/fonts')
+.copy('./node_modules/font-awesome/css/font-awesome.min.css', 'analytics/static/vendor/font-awesome');
