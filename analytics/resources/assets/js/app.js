0cc3bc2ac47b34ba6b4721f8351ba9debedf5a34
Anna Fleming <afleming@petsafe.net>
2017-05-19 18:37:26 -0400

    Import Chart.js library

--- analytics/resources/assets/js/app.js
+++ analytics/resources/assets/js/app.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import axios from 'axios';
 import router from './routes';
 import VueRouter from 'vue-router';
-
+import Chart from 'chart.js';
 
 window.Vue = Vue;
 window.axios = axios;
