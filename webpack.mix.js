14f599ac0f938797cbef9a080367065d015a3c58
Anna Fleming <afleming@petsafe.net>
2017-05-19 10:36:05 -0400

    Add bootstrap via npm

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,7 @@ let mix = require('laravel-mix').mix;
 
 
 mix.js('analytics/resources/assets/js/app.js', 'analytics/static/js')
+.copy('./node_modules/bootstrap-sass/assets/fonts/bootstrap', 'analytics/static/vendor/fonts')
 .sass('analytics/resources/assets/sass/app.scss', 'analytics/static/css')
 .copy('./node_modules/font-awesome/fonts', 'analytics/static/vendor/fonts')
 .copy('./node_modules/font-awesome/css/font-awesome.min.css', 'analytics/static/vendor/font-awesome');
