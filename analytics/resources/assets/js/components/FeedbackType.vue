5799a503443b5ed64b97c4ddea493bb7c3b6ab9a
Anna Fleming <afleming@petsafe.net>
2017-05-22 09:17:38 -0400

    Implement FeedbackType charts

--- analytics/resources/assets/js/components/FeedbackType.vue
+++ analytics/resources/assets/js/components/FeedbackType.vue
@@ -1,11 +1,70 @@
 <template>
   <div>
-    {{ info }}
+    <div class="row">
+      <div class="col-lg-12">
+              <h1 class="page-header">
+                  Charts
+                  <small>Website vs Product Feedback</small>
+              </h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title">PetSafe</h3>
+                </div>
+                <div class="panel-body">
+                  <div class="well" v-if="info.petsafe">
+                    <bar-graph :labels="info.petsafe.Keys" :values="[
+                    {
+                      data: info.petsafe['Products'],
+                      color: '#D3C4BE',
+                      name: 'Products'
+                    },{
+                      data: info.petsafe['Website Experience'],
+                      color: '#EBCFC4',
+                      name: 'Website Experience'
+                    }]"></bar-graph>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-yellow">
+                <div class="panel-heading">
+                    <h3 class="panel-title">SportDOG</h3>
+                </div>
+                <div class="panel-body">
+                  <div class="well" v-if="info.sportdog">
+                    <bar-graph :labels="info.sportdog.Keys" :values="[
+                    {
+                      data: info.sportdog['Products'],
+                      color: '#D3C4BE',
+                      name: 'Products'
+                    },{
+                      data: info.sportdog['Website Experience'],
+                      color: '#EBCFC4',
+                      name: 'Website Experience'
+                    }]"></bar-graph>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
   </div>
 </template>
 
 <script>
+import BarGraph from './graphs/BarGraph';
+
   export default{
+    components: { BarGraph },
+
     data(){
       return {
         info : {}
@@ -15,6 +74,8 @@
       axios.get('/charts/feedback_type').then(response =>{
         this.info = response.data
       });
+    },
+    methods: {
     }
   }
 </script>
