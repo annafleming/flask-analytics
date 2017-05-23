bcc0d51651651132730ac6f4e646137980acfa0d
Anna Fleming <afleming@petsafe.net>
2017-05-23 15:04:42 -0400

    Refactored LineGraph vue component to accept custom YLabels

--- analytics/resources/assets/js/components/graphs/LineGraph.vue
+++ analytics/resources/assets/js/components/graphs/LineGraph.vue
@@ -5,23 +5,50 @@
 <script>
 
   export default{
-    props: [ 'labels', 'values'],
+    props: {
+      labels: Array,
+      values: Array,
+      yrewrites: {
+        type: Object,
+        required: false
+      }
+    },
     data(){
       return {
       }
     },
     mounted(){
-      let context = this.$refs.canvas.getContext('2d');
-      var chartData = {
+      let self = this;
+      let options = {
+        legend:{
+          display: false
+        }
+      };
+      if (this.yrewrites){
+        options['scales'] = {
+            yAxes: [{
+                    ticks: {
+                        max: Math.max(...Object.keys(self.yrewrites)),
+                        callback: function(value, index, values) {
+                          return (self.yrewrites[value]) ? self.yrewrites[value] : '';
+                        }
+                    }
+                }
+            ]
+        }
+      }
+
+      let chartData = {
           labels: this.labels,
           datasets: [],
       };
+
       this.values.forEach((value, index, array) => {
         chartData.datasets.push({
           data: value ? value : [],
         });
       });
-      new Chart.Line(context, {data: chartData, options:{legend:{display: false}}});
+      new Chart.Line(this.$refs.canvas.getContext('2d'), {data: chartData, options: options});
     }
   }
 </script>
