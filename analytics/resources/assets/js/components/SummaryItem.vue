999ba88c3205784004cf1c9b70c4af2208fdf0dd
Anna Fleming <afleming@petsafe.net>
2017-05-30 08:26:06 -0400

    Change Dashboard panels layout

--- analytics/resources/assets/js/components/SummaryItem.vue
+++ analytics/resources/assets/js/components/SummaryItem.vue
@@ -3,16 +3,34 @@
       <div class="well">
           <h3><slot></slot></h3>
           <div class="row">
-              <div class="col-lg-3 col-md-6">
-                <summary-panel text="Reviews" icon="fa-comments" :value="summary['reviews']" color="primary"></summary-panel>
+              <div class="col-lg-12">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                <i class="fa fa-comments fa-4x"></i>
+                            </div>
+                            <div class="col-xs-9 text-right">
+                                <div><span class="huge">{{ summary['reviews'] }}</span></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-12 text-right">
+                                <div>Reviews</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
               </div>
-              <div class="col-lg-3 col-md-6">
+          </div>
+          <div class="row">
+              <div class="col-lg-4">
                 <summary-panel text="Promoters" icon="fa-smile-o" :value="summary['promoters']" color="green"></summary-panel>
               </div>
-              <div class="col-lg-3 col-md-6">
+              <div class="col-lg-4">
                 <summary-panel text="Passives" icon="fa-meh-o" :value="summary['passives']" color="yellow"></summary-panel>
               </div>
-              <div class="col-lg-3 col-md-6">
+              <div class="col-lg-4">
                 <summary-panel text="Detractors" icon="fa-frown-o" :value="summary['detractors']" color="red"></summary-panel>
               </div>
           </div>
