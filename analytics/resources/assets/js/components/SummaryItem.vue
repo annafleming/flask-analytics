cc795e3ec38a4dca582c37d4874ff13a07460ef6
Anna Fleming <afleming@petsafe.net>
2017-05-30 08:14:05 -0400

    Change dashboard icons to faces

--- analytics/resources/assets/js/components/SummaryItem.vue
+++ analytics/resources/assets/js/components/SummaryItem.vue
@@ -7,13 +7,13 @@
                 <summary-panel text="Reviews" icon="fa-comments" :value="summary['reviews']" color="primary"></summary-panel>
               </div>
               <div class="col-lg-3 col-md-6">
-                <summary-panel text="Promoters" icon="fa-thumbs-o-up" :value="summary['promoters']" color="green"></summary-panel>
+                <summary-panel text="Promoters" icon="fa-smile-o" :value="summary['promoters']" color="green"></summary-panel>
               </div>
               <div class="col-lg-3 col-md-6">
                 <summary-panel text="Passives" icon="fa-meh-o" :value="summary['passives']" color="yellow"></summary-panel>
               </div>
               <div class="col-lg-3 col-md-6">
-                <summary-panel text="Detractors" icon="fa-thumbs-o-down" :value="summary['detractors']" color="red"></summary-panel>
+                <summary-panel text="Detractors" icon="fa-frown-o" :value="summary['detractors']" color="red"></summary-panel>
               </div>
           </div>
       </div>
